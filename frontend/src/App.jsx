37af81c03d46cde78a2eb480c0176f00d4510f24
Marcelo Suarez <marcelo.suarez.v@gmail.com>
2023-06-10 11:48:41 +0200

    feat: react router working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,27 @@
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Onboarding from './pages/Onboarding';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 const App = () => {
   return (
     <>
-      <h1>TinPet</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path='/'
+            element={<Home />}
+          />
+          <Route
+            path='/dashboard'
+            element={<Dashboard />}
+          />
+          <Route
+            path='/onboarding'
+            element={<Onboarding />}
+          />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
