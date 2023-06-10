898e08d134c477b921be503aa6027d40c02e8d96
Marcelo Suarez <marcelo.suarez.v@gmail.com>
2023-06-10 14:19:44 +0200

    feat: account form made

--- frontend/src/components/AuthModal.jsx
+++ frontend/src/components/AuthModal.jsx
@@ -1,15 +1,71 @@
 import CloseIcon from '@mui/icons-material/Close';
+import { useState } from 'react';
 
 const AuthModal = ({ setShowModal }) => {
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [consfirmPassword, setConfirmPassword] = useState(null);
+  const [error, setError] = useState(null);
+
   const handleClick = () => {
     setShowModal(false);
   };
+  
+  // console.log(email);
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const isSignedUp = true;
 
   return (
     <div className='auth-modal'>
       <div onClick={handleClick}>
-        <CloseIcon />
+        <CloseIcon className='close-icon' />
       </div>
+      <h2>{isSignedUp ? 'CREATE AN ACCOUNT' : 'LOG IN'}</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
+        voluptates minima. Amet quod aspernatur doloremque dolorum laborum est
+        laudantium libero!
+      </p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='email'
+          id='email'
+          name='email'
+          placeholder='email'
+          required={true}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type='password'
+          id='password'
+          name='password'
+          placeholder='password'
+          required={true}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <input
+          type='password'
+          id='password-check'
+          name='password-check'
+          placeholder='confirm'
+          required={true}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+
+        <input
+          type='submit'
+          className='btn-secondary'
+        />
+        <p>{error}</p>
+      </form>
+      <hr />
+      <h2>GET THE APP</h2>
     </div>
   );
 };
