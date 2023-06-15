d6f8b9b7e499c5426d5a9453e4c8bccba22666e1
Mylena <myvendramini@gmail.com>
2023-06-15 16:25:36 +0100

    feat: add User, Dog and Matches interfaces

---
+++ server/models/Types.ts
@@ -0,0 +1,26 @@
+interface User {
+  id: number;
+  username: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface Dog {
+  id: number;
+  name: string;
+  age: number;
+  gender: string;
+  about: string;
+  url: string;
+  liked_dog: Array<number>;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface Matches {
+  id: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
