3cb06dc1222c830a22218f558284d65d8d058969
Mylena <myvendramini@gmail.com>
2023-06-15 16:52:35 +0100

    feat: create User model

---
+++ server/models/User.ts
@@ -0,0 +1,37 @@
+import { Sequelize, Model, DataTypes } from 'sequelize';
+import { IUser } from './Interfaces';
+const sequelize = new Sequelize();
+
+class User extends Model<IUser> {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'User',
+  }
+);
