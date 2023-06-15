483a8de72eeb19aae8b79a91780157f1cda21b6d
Harold Sadca <sadcah@yahoo.com>
2023-06-15 17:50:18 +0100

    Feat:Server Running

---
+++ server/compiled/routers/router.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const koa_router_1 = __importDefault(require("koa-router"));
+const router = new koa_router_1.default();
+router.post('/user',() => {
+    console.log('router')
+});
+// router.post('/dogs');
+// router.get('/user');
+// router.get('dogs');
+// router.put('/dogs');
+exports.default = router;
