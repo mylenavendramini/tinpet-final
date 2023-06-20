74629e35c4881f67abe6c2bb93b5b47d49c96033
Mylena <myvendramini@gmail.com>
2023-06-20 12:22:49 +0100

    feat: create user and create dog tests working

--- server/tests/mocks.js
+++ server/tests/mocks.js
@@ -1,136 +1,19 @@
 'use strict';
-// export const MockDog = jest.mock('../compiled/models/Dog', () => {
-//   class MockDog {
-//     id = 1;
-//     name = 'Bob';
-//     age = 3;
-//     gender = 'Male';
-//     about = 'sweet dog';
-//     url = 'https://mockdog.com';
-//     liked_dog = [];
-//     matches_dogs = [];
-//     createdAt = new Date();
-//     updatedAt = new Date();
-
-//     static associations = {
-//       user: {},
-//       matches: {},
-//     };
-
-//     static initModel = jest.fn();
-//   }
-
-//   return {
-//     Dog: MockDog,
-//   };
-// });
-
-// export const MockAnotherDog = jest.mock('../compiled/models/Dog', () => {
-//   class MockAnotherDog {
-//     id = 2;
-//     name = 'Lily';
-//     age = 2;
-//     gender = 'Female';
-//     about = 'grumpy dog';
-//     url = 'https://mockdog2.com';
-//     liked_dog = [];
-//     matches_dogs = [];
-//     createdAt = new Date();
-//     updatedAt = new Date();
-
-//     static associations = {
-//       user: {},
-//       matches: {},
-//     };
-
-//     static initModel = jest.fn();
-
-//   }
-
-//   return {
-//     Dog: MockAnotherDog,
-//   };
-// });
-
-// export const MockOneAnotherDog = jest.mock('../compiled/models/Dog', () => {
-//   class MockOneAnotherDog {
-//     id = 3;
-//     name = 'Courage';
-//     age = 7;
-//     gender = 'Male';
-//     about = 'cowardly dog';
-//     url = 'https://mockdog2.com';
-//     liked_dog = [];
-//     matches_dogs = [];
-//     createdAt = new Date();
-//     updatedAt = new Date();
-
-//     static associations = {
-//       user: {},
-//       matches: {},
-//     };
-
-//     static initModel = jest.fn();
-//   }
-
-//   return {
-//     Dog: MockOneAnotherDog,
-//   };
-// });
-
-
-
-// const MockUser = jest.mock('../compiled/models/User', () => {
-//   class MockUser {
-//     id = 1;
-//     username = 'Mike';
-//     email = 'mike@example.com';
-//     password = 'mockpassword';
-//     createdAt = new Date();
-//     updatedAt = new Date();
-
-//     static associations = {
-//       dogs: {},
-//     };
-
-//     static create = jest.fn();
-//     static initModel = jest.fn();
-//   }
-
-//   return {
-//     User: MockUser,
-//   };
-// });
-
-// export const MockAnotherUser = jest.mock('../compiled/models/User', () => {
-//   class MockAnotherUser {
-//     id = 2;
-//     username = 'Sara';
-//     email = 'sara@example.com';
-//     password = 'mockpassword';
-//     createdAt = new Date();
-//     updatedAt = new Date();
-
-//     static associations = {
-//       dogs: {},
-//     };
-
-//     static initModel = jest.fn();
-//   }
-
-//   return {
-//     User: MockAnotherUser,
-//   };
-// });
 
 const MockUser = {
-  id: 1,
   username: 'Mike',
   email: 'mike@example.com',
   password: 'mockpassword',
-  // createdAt: new Date(),
-  // updatedAt: new Date(),
+}
+
+const MockDog = {
+  name: 'Lily',
+  age: 3,
+  gender: 'female',
+  about: 'grumpy dog',
+  url: '/',
 }
 
 
-module.exports = { MockUser }
+
+module.exports = { MockUser, MockDog }
--- server/tests/test.js
+++ server/tests/test.js
@@ -1,23 +1,29 @@
 
 const { describe, expect, test } = require('@jest/globals')
-const { MockDog, MockAnotherDog, MockAnotherOneDog, MockUser, MockAnotherUser } = require('./mocks')
+const { MockDog, MockUser } = require('./mocks');
+const { createUserController, getDogsOfUser } = require('../compiled/controllers/user')
+const { createDogController } = require('../compiled/controllers/dog')
 const Sequelize = require('sequelize')
 const { initModels } = require('../compiled/models/associations');
 const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
 const Router = require('koa-router');
 const { createServer } = require('http');
-const request = require('supertest');
+const supertest = require('supertest');
+// const router = require('../compiled/routers/router');
+// let request = require('supertest');
 const app = new Koa();
 const router = new Router();
+const PORT = 3001;
 
-app
-  .use(router.routes())
-  .use(router.allowedMethods());
+router.get('/', (ctx) => {
+  ctx.body = 'Hello World';
+});
 
-server = createServer(app.callback());
-const dotenv = require('dotenv');
-dotenv.config();
-
+router.post('/hello', async (ctx) => {
+  const user = ctx.request.body;
+  ctx.body = user;
+});
 let db_test = new Sequelize(
   'dog_test',
   'postgres',
@@ -30,12 +36,74 @@ let db_test = new Sequelize(
 );
 const { Dog, User } = initModels(db_test);
 
+// router.post('/user', async (ctx) => {
+//   const user = ctx.request.body;
+//   const { username, email, password } = user;
+//   try {
+//     const newUser = await User.create({
+//       username,
+//       email,
+//       password,
+//     });
+//     ctx.body = newUser;
+//     ctx.status = 201;
+//   } catch (error) {
+//     console.log(error)
+//   }
+// });
+
+router.post('/user', createUserController);
+router.post('/dogs/:id', createDogController);
+router.get('/dogs/:id', getDogsOfUser);
+
+// router.post('/dogs/2', async (ctx) => {
+//   console.log('Start here before:')
+//   try {
+//     console.log('Start here:')
+//     const user = await User.findOne({
+//       where: { id: 2 },
+//       include: { model: Dog, as: 'dogs' },
+//     });
+//     console.log({ user })
+//     console.log(user?.dogs);
+//     const dogs = user.dogs;
+//     console.log({ dogs })
+//     ctx.body = dogs;
+//     ctx.status = 201;
+//   } catch (error) {
+//     console.log(error)
+//   }
+// });
+
+router.get('/dogs/1', (ctx) => {
+  ctx.body = 'Hello World';
+});
+
+// router.post('/user', createUserController);
+
+app
+  .use(bodyParser())
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+
+
+// server = createServer(app.callback());
+let server;
+server = app.listen(PORT);
+// request = supertest(server);
+request = supertest(app.callback());
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 let syncResponse;
 
 beforeAll(async () => {
   syncResponse = await db_test.sync();
   await db_test.authenticate();
-
+  await User.sync({ force: true });
+  await Dog.sync({ force: true });
 })
 
 afterAll(async () => {
@@ -49,12 +117,105 @@ describe('database is connected', () => {
   });
 })
 
+describe('User', () => {
+  test('Create user', async () => {
+    const mock = MockUser;
+    const response = await supertest(app.callback())
+      .post('/user',)
+      .send(mock);
+    expect(response.statusCode).toEqual(201);
+    expect(response.body.username).toEqual('Mike');
+    expect(typeof response.body.id).toEqual('number');
+  });
+})
+
+describe('Dog', () => {
+  beforeEach(async () => {
+    const mock = MockUser;
+    const response = await supertest(app.callback())
+      .post('/user',)
+      .send(mock);
+  })
+  test('Create dog', async () => {
+    const mock = MockDog;
+    const response = await supertest(app.callback())
+      .post('/dogs/1',)
+      .send(mock);
+    expect(response.statusCode).toEqual(201);
+    expect(response.body.name).toEqual('Lily');
+    expect(typeof response.body.id).toEqual('number');
+  });
+
+  test('Get dog by user id', async () => {
+    const response = await supertest(app.callback())
+      .get('/dogs/1');
+    expect(response.statusCode).toEqual(200);
+    console.log(response)
+    expect(response.body).toEqual(MockDog);
+  });
+})
+
+describe('Hello World', () => {
+  test('Get Hello world works', async () => {
+    const response = await supertest(app.callback()).get('/');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Hello World');
+  });
+
+  test('Create Hello world works', async () => {
+    const title = 'Hello World';
+    const response = await supertest(app.callback())
+      .post('/hello',)
+      .send({ title });
+    expect(JSON.parse(response.text).title).toBe(title);
+
+  });
+})
+
+
+
+// test('Create user works', async () => {
+//   const title = {
+//     id: 1,
+//     username: 'Mike',
+//     email: 'mike@example.com',
+//     password: 'mockpassword'
+//   };
+//   const response = await supertest(app.callback())
+//     .post('/user',)
+//     .send(title);
+//   expect(response.body.username).toEqual('Mike');
+// });
+
+
+
+// describe('POST /user', function () {
+//   it('user.name should return "mike"', async () => {
+//     const response = await supertest(app.callback())
+//       .post('/user')
+//       .send({
+//         username: 'Mike',
+//         email: 'mike@example.com',
+//         password: 'mockpassword'
+//       });
+//     expect(response.status).toBe(201);
+//     expect(response.body).toBe({
+//       id: 1,
+//       username: 'Mike',
+//       email: 'mike@example.com',
+//       password: 'mockpassword'
+//     });
+//   });
+// });
+
 
 // describe('get user', () => {
 //   it('should return the user', async () => {
-//     const response = await request(server)
+//     // const response = await request(server)
+//     const response = await supertest(server)
+//       // const response = await request('http://localhost:3001')
 //       .get('/user/1')
-//       .set('Accept', 'application/json')
+//     // .set('Accept', 'application/json')
 //     expect(response.status).toEqual(200);
 //     expect(response.body).toEqual(MockUser);
 //   });
