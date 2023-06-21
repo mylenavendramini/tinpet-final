cbed504842dbb377314b09c273f9df891a07b08f
Ateja <atejukas123@gmail.com>
2023-06-21 13:49:34 +0100

    still trying on the tests to work

--- frontend/tests/Home.test.tsx
+++ frontend/tests/Home.test.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../../frontend/src/pages/Home";
 import { render, screen } from "@testing-library/react";
 import { assertType, expectTypeOf } from "vitest";
+import { Context } from "../src/Context/Context";
 
 describe("Home", () => {
   it("renders", () => {
@@ -14,6 +15,15 @@ describe("Home", () => {
       </BrowserRouter>
     );
     const pet = screen.getByTestId('tinPet')
-    expect(pet).toBe('TinPet')
+    expect(pet).toBeInTheDocument()
   });
-});
+
+it('displays buttons when not authenticated', () => {
+const mockContext = {
+authenticated: false,
+updateS
+}
+})
+})
+    
+  
