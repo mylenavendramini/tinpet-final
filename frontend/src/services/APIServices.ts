aaab0a9b88f7be519c1a3f10d234a88d14ed6705
Mylena <myvendramini@gmail.com>
2023-06-22 08:31:19 +0100

    fix: image css and delete upload picture file

--- frontend/src/services/APIServices.ts
+++ frontend/src/services/APIServices.ts
@@ -66,18 +66,6 @@ const apiService = {
       .then((parsedRes) => parsedRes);
   },
 
-  // getDogsofUser: async (user_id: number): Promise<Dog[]> => {
-  //   return fetch(`${PORT}/dogs/${user_id}`, {
-  //     method: 'GET',
-  //     headers: {
-  //       'Content-type': 'application/json; charset=UTF-8',
-  //     },
-  //     credentials: 'include',
-  //   })
-  //     .then((res) => res.json())
-  //     .then((parsedRes) => parsedRes);
-  // },
-
   getMatches: async (id: number) => {
     return fetch(`${PORT}/matches/${id}`, {
       method: 'GET',
--- server/models/Message.ts
+++ server/models/Message.ts
@@ -1,65 +1,67 @@
-
 import {
   CreationOptional,
   DataTypes,
   InferCreationAttributes,
   InferAttributes,
   Model,
-  Sequelize
-} from 'sequelize'
+  Sequelize,
+} from 'sequelize';
 
 export class Message extends Model<
   InferAttributes<Message>,
   InferCreationAttributes<Message>
 > {
-  declare id?: CreationOptional<number>
-  declare content: string
-  declare sender_id: number
-  declare receiver_id: number
-  declare sender_name: string
-  declare receiver_name: string
-  declare createdAt: CreationOptional<Date>
-  declare updatedAt: CreationOptional<Date>
+  declare id?: CreationOptional<number>;
+  declare content: string;
+  declare sender_id: number;
+  declare receiver_id: number;
+  declare sender_name: string;
+  declare receiver_name: string;
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
 
   static initModel(sequelize: Sequelize): typeof Message {
-    Message.init({
-      id: {
-        type: DataTypes.INTEGER.UNSIGNED,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false
+    Message.init(
+      {
+        id: {
+          type: DataTypes.INTEGER.UNSIGNED,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull: false,
+        },
+        content: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: false,
+        },
+        sender_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+        receiver_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+        sender_name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        receiver_name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        createdAt: {
+          type: DataTypes.DATE,
+        },
+        updatedAt: {
+          type: DataTypes.DATE,
+        },
       },
-      content: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: false,
-      },
-      sender_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      },
-      receiver_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      },
-      sender_name: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      receiver_name: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      createdAt: {
-        type: DataTypes.DATE
-      },
-      updatedAt: {
-        type: DataTypes.DATE
+      {
+        sequelize,
       }
-    }, {
-      sequelize
-    })
+    );
 
-    return Message
+    return Message;
   }
 }
